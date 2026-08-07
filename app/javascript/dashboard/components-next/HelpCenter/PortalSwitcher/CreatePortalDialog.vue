1c91dc0e20b0e5eadafaca2344e5af43d6ed59ba
prsbrands <pauloprs01@gmail.com>
2026-08-07 11:00:47 -0500

    feat(branding): white label como CortexGen Chat (edição Community)
    - Configs de branding (installation_config.yml) apontando para CortexGen/cortexgen.cloud
    - Logos, favicons, PWA icons, manifest e avatar do bot com a marca CortexGen (#7C3AED)
    - Substituição de menções a Chatwoot nos locales EN, mailers, views e Super Admin
    - Remoção do widget de suporte Chatwoot, botões de upgrade/billing e cards premium bloqueados
    - Links de docs/status/termos apontando para domínios próprios

--- app/javascript/dashboard/components-next/HelpCenter/PortalSwitcher/CreatePortalDialog.vue
+++ app/javascript/dashboard/components-next/HelpCenter/PortalSwitcher/CreatePortalDialog.vue
@@ -109,7 +109,7 @@ const handleDialogConfirm = async () => {
     slug: state.slug,
     custom_domain: state.domain,
     blob_id: state.avatarBlobId || null,
-    color: '#2781F6', // The default color is set to Chatwoot brand color
+    color: '#7C3AED', // The default color is set to CortexGen brand color
   };
   await createPortal(portal);
 };
--- app/javascript/dashboard/constants/globals.js
+++ app/javascript/dashboard/constants/globals.js
@@ -38,11 +38,9 @@ export default {
     CONDENSED: 'condensed',
     EXPANDED: 'expanded',
   },
-  DOCS_URL: 'https://www.chatwoot.com/docs/product/',
-  HELP_CENTER_DOCS_URL:
-    'https://www.chatwoot.com/docs/product/others/help-center',
-  TESTIMONIAL_URL:
-    'https://testimonials.cdn.chatwoot.com/testimonial-content.json',
+  DOCS_URL: 'https://cortexgen.cloud/docs/',
+  HELP_CENTER_DOCS_URL: 'https://cortexgen.cloud/docs/help-center',
+  TESTIMONIAL_URL: 'https://cortexgen.cloud/testimonial-content.json',
   WHATSAPP_EMBEDDED_SIGNUP_DOCS_URL:
     'https://developers.facebook.com/docs/whatsapp/embedded-signup/custom-flows/onboarding-business-app-users#limitations',
   SMALL_SCREEN_BREAKPOINT: 768,
@@ -78,5 +76,4 @@ export default {
   },
 };
 export const DEFAULT_REDIRECT_URL = '/app/';
-export const META_RESTRICTION_STATUS_URL =
-  'https://status.chatwoot.com/incidents';
+export const META_RESTRICTION_STATUS_URL = 'https://metastatus.com';
